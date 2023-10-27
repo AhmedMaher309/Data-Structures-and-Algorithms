23bfecf793ac913f77c70dd39412fbab44183dd6
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2023-10-27 21:03:56 +0200

    added linkedlist

--- Data_Structures/linkedList.h
+++ Data_Structures/linkedList.h
@@ -14,6 +14,10 @@ class LinkedList{
 		Node * Head;
 		
 	public:
+		LinkedList(){
+			Head = nullptr;
+		}
+
 		Node * getHead(){
 			return this->Head;
 		}
@@ -48,9 +52,159 @@ class LinkedList{
 	        void push_front(int data) {
     			Node* newNode = new Node;
     			newNode->data = data;      
-    			newNode->next = Head;      
+    			newNode->next = Head; 
     			Head = newNode;            
-		}	
+		}
+
+		void pop_front(){
+			if(!Head)
+				return;
+			Node * temp = Head;
+			Head = Head->next;
+			delete temp;
+		}
+
+		void push_back(int item){
+			Node* newNode = new Node;
+			newNode->data = item;
+			newNode->next = nullptr;
+			if(!Head){
+				Head = newNode;
+			}
+			else{
+				Node * ptr = Head;
+				while(ptr->next){
+					ptr = ptr->next;
+				}
+				ptr->next = newNode;
+			}
+		}
+
+		void pop_back(){
+			if(!Head);
+
+			else if(!Head->next){
+				delete Head;
+				Head = nullptr;
+			}
+			else{
+				Node * ptr = Head;
+				while(ptr->next->next){
+					ptr = ptr->next;
+				}
+				Node* ptr2 = ptr->next;
+				delete ptr2;
+				ptr->next = nullptr;
+			}
+
+		}
+		
+		int front(){
+			if(!Head){
+				return -1;
+			}
+			return Head->data;
+		}
+
+		int back(){
+			if(!Head){
+				return -1;
+			}
+			else{
+				Node* ptr = Head;
+				while(ptr->next){
+					ptr = ptr->next;
+				}
+				return ptr->data;
+			}
+		}
+
+		void insert(int index, int value){
+			if(index > this->size() || index < 0){
+				cout<<"Index out of range!"<<endl;
+			}
+			else if(index == 0){
+ 				this->push_front(value);
+			}
+			else{
+				int count = 0;
+				Node*ptr = Head;
+				while(count < index-1){
+					count++;
+					ptr = ptr->next;
+				}
+				Node * newNode = new Node;
+				newNode->data = value;
+				newNode->next = ptr->next;
+				ptr->next = newNode;
+			}
+		}
+
+		void erase(int index){
+			if(index > this->size() || index < 0){
+				cout<<"Index out of range"<<endl;
+			}
+			else if(index == 0){
+				this->pop_front();
+			}
+			else{
+				int count = 0;
+				Node*prev = Head;
+				while(count < index-1){
+					count++;
+					prev = prev->next;
+				}
+				Node* current = prev->next;
+				prev->next = current->next;
+				delete current;
+			}
+		}
+
+
+		// if the index is out of range
+		// it prints "Index out of range" and returns -1
+		int value_n_from_end(int indexFromEnd){
+			if(indexFromEnd >= this->size()|| indexFromEnd < 0){
+				cout<<"Index out range"<<endl;
+				return -1;
+			}
+
+			int index = this->size() - indexFromEnd;
+			Node *ptr = Head;
+			int count = 0;
+			while(count < index-1){
+				count++;
+				ptr = ptr->next;
+			}
+			return ptr->data;
+		}
+
+		void printList(){
+			if(!Head){
+				cout<<"Linkedlist is empty"<<endl;
+				return;
+			}
+			Node* ptr = Head;
+			while(ptr){
+				cout<<ptr->data<<"->";
+				ptr = ptr->next;
+			}
+			cout<<"NULL"<<endl;
+		}
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Data_Structures/test_list.cpp
+++ Data_Structures/test_list.cpp
@@ -5,6 +5,67 @@ int main(){
 	list.push_front(5);
 	cout<<list.size()<<endl;
 	cout<<list.getHead()->data<<endl;
+	list.push_front(20);
+	cout<<list.size()<<endl;
+	list.pop_front();
+	cout<<"after delete "<<list.size()<<endl;
+	list.pop_front();
+	cout<<"after delete "<<list.size()<<endl;
+	list.push_back(100);
+	list.push_back(40);
+	list.push_back(10);
+	list.printList();
+	list.insert(1,200);
+	list.printList();
+	cout<<list.front()<<endl;
+	cout<<list.back()<<endl;
+	cout<<list.size()<<endl;
+	list.insert(1,19);
+	list.printList();
+	cout<<list.front()<<endl;
+	cout<<list.back()<<endl;
+	cout<<list.size()<<endl;
+	list.printList();
+	list.insert(3,90);
+	list.printList();
+	list.insert(6,60);
+	list.printList();
+	list.erase(5);
+	list.printList();
+	list.erase(0);
+	list.printList();
+	list.insert(0,700);
+	list.printList();
+	cout<<list.value_n_from_end(5)<<endl;
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
